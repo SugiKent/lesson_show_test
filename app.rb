f3d9a07c94b5a483aa645809110fba64d51902a2
SugiKen <ynmk1995@gmail.com>
2018-03-21 13:54:24 +0900

    配列によるid指定を追加

--- app.rb
+++ app.rb
@@ -7,7 +7,20 @@ agent.user_agent_alias = "Mac Safari 4"
 base_url = "http://0.0.0.0:3000/"
 time_stamp = Time.now.strftime("%H%M%S")
 
-for id in 13650..21663 do
+#for id in 13650..21663 do
+ids = [13702,
+16760,
+16761,
+16762,
+16763,
+16764,
+20082,
+20429,
+20430,
+20431,
+20616,
+20816,]
+ids.each do |id|
   begin
     agent.get("#{base_url}/lesson/#{id}") do |page|
       doc = Nokogiri::HTML(page.content.toutf8)
